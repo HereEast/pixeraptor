7b1855d0c643c567fc152e29293d2ea41f3732f2
Here East <nastiapiven@gmail.com>
2026-01-31 21:15:04 +0100

    feat: update tileSize range

--- src/components/controls/TileSizeRange.tsx
+++ src/components/controls/TileSizeRange.tsx
@@ -1,16 +1,28 @@
 import { InputRange } from "../ui/InputRange";
 
+import { cn } from "~/utils";
+
 import { MIN_TILE_SIZE, MAX_TILE_SIZE, TILE_SIZE_STEP } from "~/constants";
 
 interface TileSizeRangeProps {
   tileSize: number;
   setTileSize: (tileSize: number) => void;
+  disabled?: boolean;
 }
 
-export function TileSizeRange({ tileSize, setTileSize }: TileSizeRangeProps) {
+export function TileSizeRange({
+  tileSize,
+  setTileSize,
+  disabled,
+}: TileSizeRangeProps) {
   return (
     <div className="flex flex-col gap-4">
-      <div className="flex items-center gap-2 text-sm font-semibold uppercase">
+      <div
+        className={cn(
+          "flex items-center gap-2 text-sm font-semibold uppercase",
+          disabled && "opacity-50",
+        )}
+      >
         <span>Tile</span>
         <span>[{tileSize}]</span>
       </div>
@@ -21,6 +33,7 @@ export function TileSizeRange({ tileSize, setTileSize }: TileSizeRangeProps) {
           max={MAX_TILE_SIZE}
           value={tileSize}
           step={TILE_SIZE_STEP}
+          disabled={disabled}
           onChange={setTileSize}
         />
       </div>
